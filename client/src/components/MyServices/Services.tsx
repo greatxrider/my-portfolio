c015c5a27ba30fd0ca34f98a11e6a9f6f6512775
greatxrider <daligdig.jephmari@gmail.com>
2024-09-14 21:46:31 +0800

    fix Portfolio and Certificates

---
+++ client/src/components/MyServices/Services.tsx
@@ -0,0 +1,12 @@
+const Services = () => {
+    return (
+        <div className="services-content">
+            <h1 className="services-title">Services</h1>
+            <div>
+                <p>"I’m here to help you find effective solutions and overcome challenges. Using a structured design approach, I ensure that every product is thoughtfully crafted, thoroughly tested, and continuously refined. This method not only guarantees quality but also enables the creation of solutions that truly align with your business goals and drive sustainable growth.</p>
+            </div>
+        </div>
+    );
+}
+
+export default Services;
