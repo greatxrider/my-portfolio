075e90663eb2a19258349763376d37b5f01d2809
greatxrider <daligdig.jephmari@gmail.com>
2024-09-18 23:44:20 +0800

    Add certificates and files

--- client/src/components/Certificates/Certificates.tsx
+++ client/src/components/Certificates/Certificates.tsx
@@ -4,10 +4,11 @@ const Certificates = () => {
             <h1 className="certifications-title">Certifications</h1>
             <p>I am certified to serve you.</p>
             <div className="certifications-content">
-                <div className="row row-cols-1 row-cols-md-3 g-4">
-                    <div className="col">
+                <h1 className="certifications-title">IBM</h1>
+                <div className="row row-cols-1 row-cols-md-3 g-4 justify-content-center">
+                    <div className="col devOps-specialization">
                         <div className="card h-100">
-                            <img src="..." className="card-img-top" alt="..." />
+                            <img src="/src/app/assets/png/certificates/devOps-specialization.png" className="card-img-top" alt="..." />
                             <div className="card-body">
                                 <h5 className="card-title">Card title</h5>
                                 <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -17,9 +18,9 @@ const Certificates = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="col">
+                    <div className="col applied-swe">
                         <div className="card h-100">
-                            <img src="..." className="card-img-top" alt="..." />
+                            <img src="/src/app/assets/png/certificates/applied-swe.png" className="card-img-top" alt="..." />
                             <div className="card-body">
                                 <h5 className="card-title">Card title</h5>
                                 <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
@@ -29,9 +30,115 @@ const Certificates = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="col">
+                    <div className="col devOps">
                         <div className="card h-100">
-                            <img src="..." className="card-img-top" alt="..." />
+                            <img src="/src/app/assets/png/certificates/devops.png" className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                            </div>
+                            <div className="card-footer">
+                                <small className="text-body-secondary">Last updated 3 mins ago</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col fullStackIBM">
+                        <div className="card h-100">
+                            <img src="/src/app/assets/png/certificates/fullstack.png" className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                            </div>
+                            <div className="card-footer">
+                                <small className="text-body-secondary">Last updated 3 mins ago</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="certifications-content">
+                <h1 className="certifications-title">NuCamp Coding Bootcamp</h1>
+                <div className="row row-cols-1 row-cols-md-3 g-4 justify-content-center">
+                    <div className="col jsFundamentals">
+                        <div className="card h-100">
+                            <img src="/src/app/assets/png/certificates/jsFundamentals.png" className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                            </div>
+                            <div className="card-footer">
+                                <small className="text-body-secondary">Last updated 3 mins ago</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col dynamicWithReact">
+                        <div className="card h-100">
+                            <img src="/src/app/assets/png/certificates/dynamicWithReact.png" className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                            </div>
+                            <div className="card-footer">
+                                <small className="text-body-secondary">Last updated 3 mins ago</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col reactNative">
+                        <div className="card h-100">
+                            <img src="/src/app/assets/png/certificates/reactNative.png" className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                            </div>
+                            <div className="card-footer">
+                                <small className="text-body-secondary">Last updated 3 mins ago</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col backEndDev">
+                        <div className="card h-100">
+                            <img src="/src/app/assets/png/certificates/backEndDev.png" className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                            </div>
+                            <div className="card-footer">
+                                <small className="text-body-secondary">Last updated 3 mins ago</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col frontEndWeb">
+                        <div className="card h-100">
+                            <img src="/src/app/assets/png/certificates/frontEndWeb.png" className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                            </div>
+                            <div className="card-footer">
+                                <small className="text-body-secondary">Last updated 3 mins ago</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col fullStackWebandMobile">
+                        <div className="card h-100">
+                            <img src="/src/app/assets/png/certificates/fullStackWebAndMobile.png" className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                            </div>
+                            <div className="card-footer">
+                                <small className="text-body-secondary">Last updated 3 mins ago</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="certifications-content">
+                <h1 className="certifications-title">Treehouse</h1>
+                <div className="row row-cols-1 row-cols-md-3 g-4 justify-content-center">
+                    <div className="col fullStackJavascript">
+                        <div className="card h-100">
+                            <img src="/src/app/assets/png/certificates/fullStackJavascript.png" className="card-img-top" alt="..." />
                             <div className="card-body">
                                 <h5 className="card-title">Card title</h5>
                                 <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
