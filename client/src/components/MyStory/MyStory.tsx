d9c95d5ae2d8545d66246d9c2a5b6ab9bfa43745
greatxrider <daligdig.jephmari@gmail.com>
2024-09-18 20:18:08 +0800

    Add daily routine

--- client/src/components/MyStory/MyStory.tsx
+++ client/src/components/MyStory/MyStory.tsx
@@ -1,31 +1,67 @@
 const MyStory = () => {
     return (
         <div className="mystory-content">
-            <h1>ALL ABOUT ME</h1>
-            <div>
-                <h1 className="mystory-title">KEEPING THINGS ORGANIZED</h1>
-                <div>
-                    <div>
-                        <div>
-                            
+            <h1 className="mystory-title">ALL ABOUT ME</h1>
+            <div className="mystory-dailyRoutine">
+                <h1 className="mystory-dailyRoutine-title">KEEPING THINGS ORGANIZED</h1>
+                <div className="mystory-dailyRoutine-content">
+                    <div className="mystory-dailyRoutine-row">
+                        <div className="mystory-dailyRoutine-imageContainer-D">
+                            <img src="/src/app/assets/png/dailyRoutine.png" alt="Daily Routine" />
                         </div>
-                        <div>
-
+                        <div className="mystory-dailyRoutine-imageContainer-G">
+                            <img src="/src/app/assets/png/ganttChart.png" alt="Gantt Chart" />
                         </div>
                     </div>
-                    <div>
-                        <div>
-
+                    <div className="mystory-dailyRoutine-row">
+                        <div className="mystory-dailyRoutine-imageContainer-T">
+                            <img src="/src/app/assets/png/toDo.png" alt="To Do" />
                         </div>
-                        <div>
-
+                        <div className="mystory-dailyRoutine-textContainer-p">
+                            <p>For all my peers who are also on the journey of changing careers or finding their path, I believe all of you, my brothers, can relate to what I have done similar to here. It was a turning point and one of the most unforgettable enjoyable moments of my life. I truly felt alive during those times when I was just starting to change my life.</p>
                         </div>
                     </div>
                 </div>
-                <div>
-
+                <div className="mystory-dailyRoutine-summary">
+                    <p>Basically, my routine was simple:</p>
+                    <h2>EAT, SLEEP, CODE, REPEAT</h2>
                 </div>
             </div>
+            {/* <div className="mystory-workingOut">
+                <h1 className="mystory-workingOut-title">WORKING OUT</h1>
+                <div className="mystory-workingOut-content">
+                    <div className="mystory-workingOut-imageContainer">
+                        <img src="/src/app/assets/png/workingOut.png" alt="Working Out" />
+                    </div>
+                    <div className="mystory-workingOut-imageContainer">
+                        <img src="/src/app/assets/png/gym.JPG" alt="Gym" />
+                    </div>
+                    <div className="mystory-workingOut-textContainer">
+                        <p>The gym is a man’s best friend—it's both a therapist for the mind and body, healing us emotionally and physically. </p>
+                        <p>I always make time to hit the gym because working out is a part of my life and always will be. </p>
+                        <p>It helps me solve problems, cope with challenges, and wipe away my tears. As long as I keep a balance and don’t let ego take over, it remains a powerful force in my life.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="mystory-workSetup">
+                <h1 className="mystory-workSetup-title">MY WORK SETUP</h1>
+                <div className="mystory-workSetup-content">
+                    <div className="mystory-workSetup-imageContainer">
+                        <img src="/src/app/assets/png/workSetup.JPG" alt="Working Out" />
+                    </div>
+                    <div className="mystory-workSetup-imageContainer">
+                        <img src="/src/app/assets/png/hero.JPG" alt="Working Out" />
+                    </div>
+                    <div className="mystory-workSetup-textContainer">
+                        <p>When a carpenter has high-quality and complete tools, the house will be well-built, and the owner will feel comfortable living there.</p>
+                        <p>I take the same approach with my work setup because it affects how well I do my job for my clients or boss.</p>
+                        <p>A good setup means more productivity, which leads to better work.</p>
+                        <p>With a fast computer, an ergonomic chair, and a proper desk, I can work all day without feeling burnt out. </p>
+                        <p>I can even work overtime and still manage my stress.</p>
+                        <p>Most importantly, a good work environment isn’t complete without a work buddy. He helps me manage stress and reminds me to take breaks when I need them.</p>
+                    </div>
+                </div>
+            </div> */}
         </div>
     );
 }
