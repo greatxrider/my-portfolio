46dcbdf6d32e55d32269a59e4cee86da3c29ac8c
greatxrider <daligdig.jephmari@gmail.com>
2024-09-18 21:06:46 +0800

    ADD MyStory component

--- client/src/components/MyStory/MyStory.tsx
+++ client/src/components/MyStory/MyStory.tsx
@@ -3,7 +3,7 @@ const MyStory = () => {
         <div className="mystory-content">
             <h1 className="mystory-title">ALL ABOUT ME</h1>
             <div className="mystory-dailyRoutine">
-                <h1 className="mystory-dailyRoutine-title">KEEPING THINGS ORGANIZED</h1>
+                <h1 className="mystory-dailyRoutine-title">I KEEP THINGS ORGANIZED</h1>
                 <div className="mystory-dailyRoutine-content">
                     <div className="mystory-dailyRoutine-row">
                         <div className="mystory-dailyRoutine-imageContainer-D">
@@ -27,41 +27,46 @@ const MyStory = () => {
                     <h2>EAT, SLEEP, CODE, REPEAT</h2>
                 </div>
             </div>
-            {/* <div className="mystory-workingOut">
-                <h1 className="mystory-workingOut-title">WORKING OUT</h1>
+            <div className="mystory-workingOut">
+                <h1 className="mystory-workingOut-title">I WORK OUT</h1>
                 <div className="mystory-workingOut-content">
-                    <div className="mystory-workingOut-imageContainer">
-                        <img src="/src/app/assets/png/workingOut.png" alt="Working Out" />
+                    <div className="mystory-workingOut-row">
+                        <div className="mystory-workingOut-imageContainer-w">
+                            <img src="/src/app/assets/png/gym.JPG" alt="Working Out" />
+                        </div>
+                        <div className="mystory-workingOut-imageContainer-g">
+                            <img src="/src/app/assets/png/9025.jpg" alt="Gym" />
+                        </div>
                     </div>
-                    <div className="mystory-workingOut-imageContainer">
-                        <img src="/src/app/assets/png/gym.JPG" alt="Gym" />
-                    </div>
-                    <div className="mystory-workingOut-textContainer">
-                        <p>The gym is a man’s best friend—it's both a therapist for the mind and body, healing us emotionally and physically. </p>
-                        <p>I always make time to hit the gym because working out is a part of my life and always will be. </p>
-                        <p>It helps me solve problems, cope with challenges, and wipe away my tears. As long as I keep a balance and don’t let ego take over, it remains a powerful force in my life.</p>
+                    <div className="mystory-workingOut-row">
+                        <div className="mystory-workingOut-textContainer-pw">
+                            <p>The gym is a man’s best friend—it's both a therapist for the mind and body, healing us emotionally and physically. </p>
+                            <p>I always make time to hit the gym because working out is a part of my life and always will be. </p>
+                            <p>It helps me solve problems, cope with challenges, and wipe away my tears. As long as I keep a balance and don’t let ego take over, it remains a powerful force in my life.</p>
+                        </div>
                     </div>
                 </div>
             </div>
             <div className="mystory-workSetup">
                 <h1 className="mystory-workSetup-title">MY WORK SETUP</h1>
                 <div className="mystory-workSetup-content">
-                    <div className="mystory-workSetup-imageContainer">
-                        <img src="/src/app/assets/png/workSetup.JPG" alt="Working Out" />
+                    <div className="mystory-workSetup-row">
+                        <div className="mystory-workSetup-imageContainer-ws">
+                            <img src="/src/app/assets/png/workSetup.JPG" alt="Work Setup" />
+                        </div>
+                        <div className="mystory-workSetup-imageContainer-h">
+                            <img src="/src/app/assets/png/hero.JPG" alt="Hero" />
+                        </div>
                     </div>
-                    <div className="mystory-workSetup-imageContainer">
-                        <img src="/src/app/assets/png/hero.JPG" alt="Working Out" />
-                    </div>
-                    <div className="mystory-workSetup-textContainer">
-                        <p>When a carpenter has high-quality and complete tools, the house will be well-built, and the owner will feel comfortable living there.</p>
-                        <p>I take the same approach with my work setup because it affects how well I do my job for my clients or boss.</p>
-                        <p>A good setup means more productivity, which leads to better work.</p>
-                        <p>With a fast computer, an ergonomic chair, and a proper desk, I can work all day without feeling burnt out. </p>
-                        <p>I can even work overtime and still manage my stress.</p>
-                        <p>Most importantly, a good work environment isn’t complete without a work buddy. He helps me manage stress and reminds me to take breaks when I need them.</p>
+                    <div className="mystory-workSetup-row">
+                        <div className="mystory-workSetup-textContainer-pws">
+                            <p>When a carpenter has the right tools, the house is well-built and comfortable. The same goes for my work setup—it directly impacts my productivity and the quality of my work.</p>
+                            <p>With a fast computer, ergonomic chair, and good desk, I can work efficiently without burning out, even during overtime.</p>
+                            <p>And of course, having a work buddy to help manage stress and remind me to take breaks is key to staying balanced.</p>
+                        </div>
                     </div>
                 </div>
-            </div> */}
+            </div>
         </div>
     );
 }
