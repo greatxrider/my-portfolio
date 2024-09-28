ecdfd7328c06f2555d22cf83ad5029a1f4898cf5
Jeph Mari Daligdig <122143538+greatxrider@users.noreply.github.com>
2024-09-29 01:50:44 +0800

    update database

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  // setup a friendly greeting for the root route
+  res.json({
+    message: 'Welcome to the REST API project!',
+  });
 });
 
 module.exports = router;
