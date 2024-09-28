7e393c0bf3690ea90fb5c4b92ead02c727ed3e14
Jeph Mari Daligdig <122143538+greatxrider@users.noreply.github.com>
2024-09-29 01:42:08 +0800

    latest update decided to put onto public

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,10 +4,6 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Specify the output directory
-    assetsDir: 'static/assets', // Specify the assets directory within the output directory
-  },
   server: {
     open: true,
   },
